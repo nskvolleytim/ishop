4ddf608cc32e58906318eee14c0fe9d788c33b44
Timson <nskvolleytim@gmail.com>
2018-04-24 18:47:38 +0600

    displaying products on the catlog page by category

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,10 +1,19 @@
 <?php 
 
+	include (ROOT.'/models/Category.php');
+	include (ROOT.'/models/Product.php');
+
 	class SiteController
 	{
 
 		public function actionIndex()
 		{
+			$categories = array();
+			$categories = Category::getCategoriesList();
+
+			$latestProducts = array();
+			$latestProducts = Product::getLatestProducts(6);
+
 			require_once(ROOT.'/views/site/index.php');
 
 			return true;
